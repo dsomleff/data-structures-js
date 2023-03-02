ceb260b7f8a90c4ebf5a430ac51a11f5b76dfc94
somleff <somleff@gmail.com>
2023-03-02 13:16:15 +0100

    BTS CContains Method

--- trees/v1/index.js
+++ trees/v1/index.js
@@ -37,5 +37,22 @@ class BST {
             }
         }
     }
+
+    contains(value){
+        if(this.root === null) return false;
+        let temp = this.root;
+
+        while(temp) {
+            if(value < temp.value){
+                temp = temp.left;
+            } else if(value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
