e0b80f3860954e3240d886a12dd3008e966f23b4
somleff <somleff@gmail.com>
2023-03-06 14:48:50 +0100

    Hash Tables Constructor

---
+++ hash-tables/index.js
@@ -0,0 +1,19 @@
+class HashTable {
+    constructor(size = 7) {
+        this.dataMap = new Array(size);
+    }
+
+    /**
+     * method that should only
+     * be called by other methods.
+     */
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+        }
+        return hash;
+    }
+}
+
+let myHashTable = new HashTable();
