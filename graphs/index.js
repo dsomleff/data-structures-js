5aa181fe0c332e4c15876e3e8d16316995e66c1f
somleff <somleff@gmail.com>
2023-03-08 14:06:27 +0100

    Graph removeEdge

--- graphs/index.js
+++ graphs/index.js
@@ -21,9 +21,26 @@ class Graph {
 
         return false;
     }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2 );
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v =>  v !== vertex1 );
+
+            return true;
+        }
+        return false;
+    }
 }
 
 let myGraph = new Graph();
-myGraph.addVertex(1);
-myGraph.addVertex(2);
-myGraph.addEdge('1', '2');
+myGraph.addVertex('A');
+myGraph.addVertex('B');
+myGraph.addVertex('C');
+
+myGraph.addEdge('A', 'B');
+myGraph.addVertex('B', 'C');
+myGraph.addVertex('C', 'A');
+
+myGraph.removeEdge('A', 'B');
+myGraph.removeEdge('A', 'D');
