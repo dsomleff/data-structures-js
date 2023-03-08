8cf0f07d939be0d988467ae3e295136e221d9cf5
somleff <somleff@gmail.com>
2023-03-08 13:54:44 +0100

    Graph addEdge

--- graphs/index.js
+++ graphs/index.js
@@ -11,7 +11,19 @@ class Graph {
 
         return false;
     }
+
+    addEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push(vertex2);
+            this.adjacencyList[vertex2].push(vertex1);
+            return true;
+        }
+
+        return false;
+    }
 }
 
 let myGraph = new Graph();
-myGraph.addVertex('A');
+myGraph.addVertex(1);
+myGraph.addVertex(2);
+myGraph.addEdge('1', '2');
