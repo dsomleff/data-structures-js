ebc9cd95bff1b366c2ce48eb64abb22705f1d0d5
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-12 17:04:19 +0200

    Linked List: condition

---
+++ my_own/linked_list/README.md
@@ -0,0 +1,18 @@
+```js
+// Linked List is a series of nodes linked together
+// Node could be {}, [], '', number.
+Head -> Node -> Tail -> null
+```
+Node contains:
+```js
+data: {}
+next: // pointer to the next node
+```
+
+Pros
+- Super fast insert/delete - O(1) <br>
+
+Cons:
+- No indexes as in array
+- Get / Set is linear or O(n)
+
---
+++ my_own/linked_list/index.ts
@@ -0,0 +1,16 @@
+interface LinkedList {
+    // length(): number;
+    // insertAt(item: any, index: number): void;
+    // remove(item: T): T | undefined;
+    // removeAt(index: number): T | undefined;
+    // append(item: T): void;
+    // prepend(item: T): void;
+    // get(index: number): T | undefined
+}
+
+class LinkedList implements LinkedList {
+
+}
+
+const myLinky = new LinkedList();
+console.log(myLinky);
