6270afe911bc0da119968092c24714c2f9f4a3a2
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-12 13:46:40 +0200

    Bubble Sort: solution

---
+++ my_own/bubble_sort/index.ts
@@ -0,0 +1,15 @@
+function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length - 1; ++i) {
+        for (let j = i + 1; j < arr.length; ++j) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
+
+const sortMe = [5, 3, 8, 2, 1, 4];
+console.log(bubble_sort(sortMe));
