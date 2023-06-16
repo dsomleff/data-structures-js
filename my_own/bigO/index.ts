80a020c9007d95d12b70cc358a48fe978866985e
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-06-16 14:52:47 +0200

    Queue: - condition

--- my_own/bigO/index.ts
+++ my_own/bigO/index.ts
@@ -20,7 +20,7 @@ logAll([1, 2, 3, 4, 5, 6]);
 logAll([1, 2, 3, 4, 5, 6, 7]);
 
 
-// Exponential runtime - Big O Notation: "O (n^2)"
+// Exponential runtime - Big O Notation: "O (n^2)" || "O (2n) -> recursion"
 function addAndLog(array: string[]): void {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
@@ -35,5 +35,6 @@ addAndLog(['A', 'B', 'C', 'D', 'E']);  // 25 pairs logged out
 
 
 // Logarithmic runtime - Big O Notation: O (log n)
+// when doubling the number of elements doesn't double the time
 function binarySearch(array: number[], key: number) {}
 binarySearch([1, 2, 3, 4], 3);
---
+++ my_own/queue/README.md
@@ -0,0 +1,5 @@
+- Queue is DS build on top of the LinkedList.
+- Main feature is first in first out
+- Logic is limited by:
+    - always add node to the tail (add)
+    - always remove node from the head (remove)
