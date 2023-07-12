3ca4ceab895972c4b76797cadbc3945d0386348f
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-07-12 13:28:36 +0200

    BFS: - Solution

--- my_own/trees/depth_first_traversals/types.ts
+++ my_own/trees/depth_first_traversals/types.ts
@@ -1,7 +1,7 @@
 type BinaryNode<T> = {
     value: T,
-    left: BinaryNode<T>,
-    right: BinaryNode<T>
+    left: BinaryNode<T> | null,
+    right: BinaryNode<T> | null,
 };
 
 export default BinaryNode;
