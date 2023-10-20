cfc570fda9e8529386da2aa7180094c68636b0fc
Dmytro Somliev <dmytro.somliev@ibm.com>
2023-10-20 10:50:07 +0200

    Graphs: - BFS for Adjacency Matrix

---
+++ my_own/graphs/BFSGrapgMatrix.ts
@@ -0,0 +1,51 @@
+declare type WeightedAdjacencyMatrix = number[][] // Number means weight
+
+/**
+ * Return the path that we took while looking for a needle
+ */
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number):number[] | null {
+    const seen = new Array(graph.length).fill(false); // all visited nodes
+    const prev = new Array(graph.length).fill(-1); // from where we came from to this node
+
+    seen[source] = true;
+    const q: number[] = [source]; // our queue of visited nodes
+
+    do {
+        const curr = q.shift() as number; // pop up current node from the q
+
+        if (curr === needle) {
+            break; // found it!
+        }
+
+        const adjs = graph[curr];
+        for (let i = 0; i < adjs.length; i++) {
+            if (adjs[i] === 0) {
+                continue; // no edge at this point
+            }
+
+            if (seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+    } while (q.length);
+
+    if (prev[needle] === -1) {
+        return null;
+    }
+
+    // build it backwards
+    let curr = needle;
+    const out: number[] = []; // path from the needle to the source
+
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr]; // who added me to the search
+    }
+
+    return [source].concat(out.reverse());
+}
+
